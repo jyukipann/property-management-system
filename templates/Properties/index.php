50431c537909f992273c7c76efa54f07bdb8576e
jyukipann <jyukipann@yahoo.co.jp>
2023-10-07 19:59:45 +0900

    検索機能のためのフォーム作成

--- templates/Properties/index.php
+++ templates/Properties/index.php
@@ -1,9 +1,36 @@
+<script>
+    function toggleDisplay(id="search_form"){
+        let current = document.getElementById(id).style.display;
+        let setTo = "none";
+        if(current == "none"){
+            setTo = "block";
+        }else if(current == "block"){
+            setTo = "none";
+        }
+        document.getElementById(id).style.display = setTo;
+    }
+</script>
 <div class="container">
+    <div class="content" id="search_form">
+        <div class="properties form">
+            <?= $this->Form->create(null, ['type' => 'get', 'url' =>['action' => 'index','controller' => 'Properties']]); ?>
+            <fieldset>
+                <legend>
+                    <?= __('物件の検索') ?>
+                </legend>
+                <?= $this->Form->control('エリア') ?>
+                <?= $this->Form->control('予算') ?>
+            </fieldset>
+            <?= $this->Form->button(__('検索')); ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+    <hr style="visibility: hidden;">
     <div class="content">
-    <h2> 物件一覧 </h2>
-    <?php
-    foreach ($properties as $property) {
-        echo <<< EOM
+        <h2> 物件一覧 </h2><button onclick="toggleDisplay();">検索機能</button>
+        <?php
+        foreach ($properties as $property) {
+            echo <<<EOM
         <hr>
         <div class="row">
             <div class="column">
@@ -16,7 +43,7 @@
             </div>
         </div>
         EOM;
-    }
-    ?>
+        }
+        ?>
     </div>
 </div>
